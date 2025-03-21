8969b55c80aea6f120dff919db570362e48bdd91
Calvin Kwong <calvinkwong@verily.com>
2025-03-21 14:07:26 -0700

    add strapi content source map plugin to enable visual editing

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,2 +1,10 @@
 export default () => ({
+    'content-source-map': {
+        enabled: true,
+        config: {
+            contentTypes: ['api::hero.hero'],
+            origin: 'verily-strapi-vercel-demo.vercel.app',
+            baseHref: 'https://meaningful-nest-ba40904bc9.strapiapp.com',
+        },
+    },
 });
